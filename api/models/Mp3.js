8e5c8ec6af2098aa5fb72aef2172115ff65eb024
Shake <fidalgo.antoine@gmail.com>
2016-05-19 15:51:16 +0200

    model mp3 maj

--- api/models/Mp3.js
+++ api/models/Mp3.js
@@ -1,14 +1,25 @@
 /**
  * Mp3.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ * @description :: TODO: You might write a short summary of how this model works
+ * and what it represents here.
+ * @docs        ::
+ * http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
+  Title,
+  Artist,
+  Year,
+  Genre attributes : {
 
-  attributes: {
+    title : {type : 'string', defaultsTo : 'unknow'},
 
-  }
-};
+    album : {type : 'string', defaultsTo : 'unknow'},
 
+    artist : {type : 'string', defaultsTo : 'unknow'},
+
+    year : {type : 'string', defaultsTo : 'unknow'},
+
+    genre : {type : 'string', defaultsTo : 'unknow'},
+  };
